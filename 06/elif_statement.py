2d32f416ea2b09ea6a8595bce0075bedb2f6270f
Shahidkhan Pathan <page.shahidkhan@gmail.com>
2025-06-28 18:06:34 +0530

    Day-06 files added

---
+++ 06/elif_statement.py
@@ -0,0 +1,8 @@
+# elif is short for "else if" and is used to check multiple conditions in a sequence.
+x = 10
+if x > 15:
+    print(f"{x} is greater than 15")
+elif x > 5:
+    print(f"{x} is greater than 5 but not greater than 15")
+else:
+    print(f"{x} is not greater than 5")
---
+++ 06/else_statement.py
@@ -0,0 +1,6 @@
+a = 10
+
+if a > 5:
+    print(f"{a} is greater than 5")
+else:
+    print(f"{a} is not greater than 5")
